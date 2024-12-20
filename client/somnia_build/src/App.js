c826198a85729aec0a480aedac7c0a5c687aee93
Park Seungjae <andrewpark0619@gmail.com>
2024-12-20 17:07:30 +0900

    Learn React 링크 삭제

--- client/somnia_build/src/App.js
+++ client/somnia_build/src/App.js
@@ -14,15 +14,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hellow world!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h1>Hellow World</h1>
       </header>
     </div>
   );
