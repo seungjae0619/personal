7fb86e89d3f2e79bccb25440f02cd8791ba628af
Park Seungjae <andrewpark0619@gmail.com>
2024-12-20 17:59:24 +0900

    react 수정 테스트 2

--- client/somnia_build/src/App.js
+++ client/somnia_build/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hellow world!</p>
+        <p>Hello world!</p>
       </header>
     </div>
   );
