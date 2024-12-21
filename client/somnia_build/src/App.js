00375e3bdacd04d197094e5d5b5aaa50947432db
Park Seungjae <andrewpark0619@gmail.com>
2024-12-21 12:49:39 +0900

    andy

--- client/somnia_build/src/App.js
+++ client/somnia_build/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world!</p>
+        <p>Hello world! Andrew</p>
       </header>
     </div>
   );
