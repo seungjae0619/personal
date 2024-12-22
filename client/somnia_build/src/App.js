819bab374516ea13c6b38e715e6df2c433e8ea74
Park Seungjae <andrewpark0619@gmail.com>
2024-12-22 12:19:37 +0900

    test commit

--- client/somnia_build/src/App.js
+++ client/somnia_build/src/App.js
@@ -4,16 +4,17 @@ import "./App.css";
 
 function App() {
   useEffect(() => {
-    fetch("http://localhost:3000/")
+    fetch("http://localhost:8080/")
       .then((res) => res.json())
       .then((data) => console.log(data));
-  });
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello world! Andrew</p>
+        <h2>This is my project</h2>
       </header>
     </div>
   );
